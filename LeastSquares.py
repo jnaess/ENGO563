6ddbec6eae5a301f2829e92e1f8ce709d0ab9a20
jnaess <Jan Erik Naess>
2021-09-16 13:20:04 -0500

    least squares (LS) class now auto reads in the list of unknowns and knowns and assigns them to the required class variables

--- LeastSquares.py
+++ LeastSquares.py
@@ -8,184 +8,58 @@ class LS():
     """
         Holds the universal values needed to integrate the different LS adjustments into one
     """
-    def __init__(self):
+    def __init__(self, file_name = "coords.txt"):
         """
         Desc:
-            
+            reads in the list of knowns and unknowns and assigns their values. Will construct design matrix, etc. based off of these
         Input:
         Output:
             sets up u_list (predefined in here)
             sets up number of unknowns (self.u)
             
-                    self.u_list = ['SA10_E', "SA10_N", "SA10_H", 
-                        'UCAL_E', "UCAL_N", "UCAL_H", 
-                         'ASCM1_E', "ASCM1_N", "ASCM1_H",
-                        'BASE_E', "BASE_N", "BASE_H",
-                        'C1_E', "C1_N", "C1_H",
-                        'C2_E', "C2_N", "C2_H",
-                        'C3_E', "C3_N", "C3_H",
-                      'N_E', "N_N", "N_H",
-                      'S_E', "S_N", "S_H"]
         """
-        
-        #_______________________________________________________________
-                #N, E, H linear
-        self.u_list = ['SA10_E', "SA10_N", "SA10_H", 
-                        'UCAL_E', "UCAL_N", "UCAL_H",
-                       'ASCM1_E', "ASCM1_N","ASCM1_H",
-                        'BASE_E', "BASE_N", "BASE_H",
-                        'C1_H',
-                        'C2_E', "C2_N", "C2_H",
-                        'C3_E', "C3_N", "C3_H",
-                      "N_H",
-                      "S_H"]
-        #N, E, H linear only
-        self.x_0 = t(mat([-9292.095, 5660358.617, 1136.040,#SA10
-                         -9378.481, 5660425.221, 1135.642,#UCAL
-                          -9903.780, 5659098.252, 1099.627,#ASCM1
-                         -9245.156, 5660419.921, 1111.683,#BASE
-                         1111.207, #C1
-                         -9492.719, 5660518.107, 1112.521, #C2
-                         -9428.997, 5660514.752, 1114.125,#C3
-                         1135.959, #N
-                         1135.887 ]))#S
-        
-        #ASCM2_H, C1E, C1N
-        self.c = t(mat([1110.234, -9000, 566000]))
-        #_______________________________________________________________
-        #_______________________________________________________________
-                #H linear
-        self.u_list = [
-            "SA10_H", 
-                        "UCAL_H",
-                       "ASCM1_H",
-                        "BASE_H",
-                        'C1_H',
-                        "C2_H",
-                        "C3_H",
-                      "N_H",
-                      "S_H"]
-        #N, E, H linear only
-        self.x_0 = t(mat([ 
-                            1136.040,#SA10
-                          1135.642,#UCAL
-                          1099.627,#ASCM1
-                          1111.683,#BASE
-                         1111.207, #C1
-                          1112.521, #C2
-                          1114.125,#C3
-                         1135.959, #N
-                         1135.887 ]))#S
-        
-        #ASCM2_H, C1E, C1N
-        self.c = t(mat([1110.234, -9000, 566000]))
-        #_______________________________________________________________
-        #_______________________________________________________________
-                #E N linear
-        self.u_list = ['SA10_E', "SA10_N",  
-                        'UCAL_E', "UCAL_N",
-                       'ASCM1_E', "ASCM1_N",
-                        'BASE_E', "BASE_N", 
-                        'C2_E', "C2_N", 
-                        'C3_E', "C3_N", 
-                    ]
-        #N, E, H linear only
-        self.x_0 = t(mat([-9292.095, 5660358.617, #SA10
-                         -9378.481, 5660425.221, #UCAL
-                          -9903.780, 5659098.252,#ASCM1
-                         -9245.156, 5660419.921, #BASE
-                          #C1
-                         -9492.719, 5660518.107,  #C2
-                         -9428.997, 5660514.752, #C3
-                         
-                         ]))#S
-        
-        #ASCM2_H, C1E, C1N
-        self.c = t(mat([1110.234, -9000, 566000]))
-        
-        self.datums = [
-            "ASCM2_H",
-            "C1_E",
-            "C1_N",
-            "1_solo"
-        ]
-        #_______________________________________________________________   
-        #_______________________________________________________________
-        #H linear (leveling only)
-        self.u_list = [
-                        'C1_H',
-                        "C2_H",
-                        "C3_H",]
-        #N, E, H linear only
-        self.x_0 = t(mat([
-                         1111.207, #C1
-                          1112.521, #C2
-                          1114.125,#C3
-        ]))#S
-        
-        #ASCM2_H, C1E, C1N
-        self.c = t(mat([1110.234, -9000, 566000]))
-        
-        self.datums = [
-            "ASCM2_H"]
-        #_______________________________________________________________ 
-        #_______________________________________________________________
-        #H linear (leveling and static
-        self.u_list = [
-                        "SA10_H", 
-                        "UCAL_H",
-                       "ASCM1_H",
-                        "BASE_H",
-                        'C1_H',
-                        "C2_H",
-                        "C3_H",
-                      "N_H",
-                      "S_H"]
-        #N, E, H linear only
-        self.x_0 = t(mat([ 
-                            1136.040,#SA10
-                          1135.642,#UCAL
-                          1099.627,#ASCM1
-                          1111.683,#BASE
-                         1111.207, #C1
-                          1112.521, #C2
-                          1114.125,#C3
-                         1135.959, #N
-                         1135.887 ]))#S
-        
-        #ASCM2_H, C1E, C1N
-        self.c = t(mat([1110.234, -9000, 566000]))
-        
-        self.datums = [
-            "ASCM2_H"]
-        #_______________________________________________________________
-                #_______________________________________________________________
-                #N, E, H linear
-        self.u_list = ['SA10_E', "SA10_N", "SA10_H", 
-                        'UCAL_E', "UCAL_N", "UCAL_H",
-                       'ASCM1_E', "ASCM1_N","ASCM1_H",
-                        'BASE_E', "BASE_N", "BASE_H",
-                        'C1_H',
-                        'C2_E', "C2_N", "C2_H",
-                        'C3_E', "C3_N", "C3_H",
-                      "N_H",
-                      "S_H"]
-        #N, E, H linear only
-        self.x_0 = t(mat([-9292.095, 5660358.617, 1136.040,#SA10
-                         -9378.481, 5660425.221, 1135.642,#UCAL
-                          -9903.780, 5659098.252, 1099.627,#ASCM1
-                         -9245.156, 5660419.921, 1111.683,#BASE
-                         1111.207, #C1
-                         -9492.719, 5660518.107, 1112.521, #C2
-                         -9428.997, 5660514.752, 1114.125,#C3
-                         1135.959, #N
-                         1135.887 ]))#S
-        
-        #ASCM2_H, C1E, C1N
-        self.c = t(mat([1110.234, -9000, 566000]))
-        #_______________________________________________________________
-        self.u = len(self.u_list)
-        
+        self.file_name = file_name
+        self.read_2D()
+
+    def read_2D(self):
+        """
+        Desc:
+            reads in the 2D set of points and assigns values
+            expects format of [name easting northing known/unknown]
+        Input:
+            self.file_name
+        Output:
+            self.u_list (string list of unknown)
+            self.x_0 (initial guesses of unknowns)
+            self.c (constant values of knowns)
+            self.datums (string list of knowns)
+        """
+        df = pd.read_csv(self.file_name, sep = ' ')
+        #currently only formatted for 2D
+
+        self.u_list = []
+        self.x_0 = []
+        self.c = []
+        #pretty sure datums aren't actually used
+        self.datums = []
+
+        #assign values
+        for index, row in df.iterrows():
+            #check if known or unknown
+            if row[3] == "u":
+                #unknown name
+                self.u_list.append(row[0])
+
+                #add unknown values in order of x, y
+                self.x_0.append(row[1])
+                self.x_0.append(row[2])
+            else: #then they are "n" --> knowns
+                #known name
+                self.datums.append(row[0])
+
+                #add known values in order of x, y
+                self.c.append(row[1])
+                self.c.append(row[2])
     def find_col(self, dimension, point_name, li = "u"):
         """
         Desc:
